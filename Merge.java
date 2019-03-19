d830bcd84a42ed358de1e61475bec4d9c793a8ad
Ali Taoube <ataoube00@stuy.edu>
2019-03-19 16:06:04 -0400

    Fixed merge and edited toString

--- Merge.java
+++ Merge.java
@@ -1,9 +1,11 @@
 public class Merge{
   /*sort the array from least to greatest value. This is a wrapper function*/
   public static void main(String[] args) {
-    int[] data1 = {2,3, 4};
-    int[] data2 = {10, 9, 13};
-    System.out.print(merge());
+    int[] data1 = {1, 3, 5};
+    int[] data2 = {2, 4, 6};
+    int[] output = new int[6];
+
+    System.out.print(toString(merge(data1, data2, output)));
   }
   public static void mergesort(int[]data){
 
@@ -15,21 +17,42 @@ public class Merge{
   //   mergeH(data, )
   // }
 
-  private static int[] merge(int[] data1, int[] data2, int[] output){
-    int idx = 0;
-    for (int x = 0; x < data1.length; x++) {
-      if (data1[x] > data2[idx]) output[x] = data1[x];
-      else{
-        output[x] = data2[idx];
-        idx++;
+  private static int[] merge(int[] left, int[] right, int[] data){
+    int x = 0;
+    int y = 0;
+
+    for (int track = 0; track < data.length; track++){
+      if (x < left.length && y < right.length){
+        if (left[x] < right[y]){
+          data[track] = left[x];
+          x++;
+        }
+        else{
+          data[track] = right[y];
+          y++;
+        }
+      }
+      else if (x < left.length){
+        data[track] = left[x];
+        x++;
+      }
+      else if (y < right.length){
+        data[track] = right[y];
+        y++;
       }
     }
-  }
+    return data;
+    }
 
-  public static toString(int[] data){
+  public static String toString(int[] data){
     String output = "";
+    int y = 0;
     for (int x: data){
       output += x;
+      if (y < data.length - 1){
+        output += ", ";
+        y++;
+      }
     }
     return output;
   }
